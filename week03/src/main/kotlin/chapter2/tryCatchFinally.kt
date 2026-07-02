27e2a02b1399cdffd0fab2037e3660b9de1f17b9
gmin <gllom7884@gmail.com>
2026-07-02 15:00:57 +0900

    스터디 완

--- week03/src/main/kotlin/chapter2/tryCatchFinally.kt
+++ week03/src/main/kotlin/chapter2/tryCatchFinally.kt
@@ -24,3 +24,52 @@ private fun main(){
 
     println(value)
 }
+
+private fun test(): Int {
+    return try {
+        println("try 블록 실행")
+        val result = 10 / 0
+        result
+    } catch (e: Exception) {
+        println("예외 발생: ${e.message}")
+        -1
+    } finally {
+        println("finally 실행 (항상 실행됨)")
+    }
+
+    /*val num = try {
+        println("try 블록 실행")
+        val result = 10 / 0
+        result
+    } catch (e: Exception) {
+        println("예외 발생: ${e.message}")
+        -1
+    } finally {
+        println("finally 실행 (항상 실행됨)")
+    }
+
+    return num*/
+}
+
+private fun final(){
+    try {
+        println("try")
+    } catch (e: Exception) {
+        println("catch")
+    } finally {
+        println("finally")
+    }
+
+    /*try {
+        try {
+            println("try")
+        } catch (e: Exception) {
+            println("catch")
+        }
+
+        println("finally")
+    } catch (t: Throwable) {
+        println("finally")
+        throw t
+    }*/
+}
